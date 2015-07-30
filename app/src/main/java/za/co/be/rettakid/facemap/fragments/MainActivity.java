8599f46d1617b97352523fe77b6beeb8005179a8
lwazi8 <lwazip2@discovery,co.za>
2015-07-30 21:48:44 +0200

    creating project structure

---
+++ app/src/main/java/za/co/be/rettakid/facemap/fragments/MainActivity.java
@@ -0,0 +1,14 @@
+package za.co.be.rettakid.facemap.fragments;
+
+import android.app.Activity;
+
+import org.androidannotations.annotations.EActivity;
+
+import za.co.be.rettakid.facemap.R;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends Activity {
+
+
+
+}
